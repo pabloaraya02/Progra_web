b7f2a77842fa558d2bee5f1ab4579598e0275a5c
pabloAraya2 <pabloarayane0211@gmail.com>
2017-04-08 03:43:57 -0600

    CChanges on home view and layouts

---
+++ elearning/public/js/myJs.js
@@ -0,0 +1,7 @@
+    function hideSide(){
+    	if (document.getElementById("wrapper").className == "NotToggled"){
+    		document.getElementById("wrapper").className = "toggled"
+    	} else{
+    		document.getElementById("wrapper").className = "NotToggled";
+    	}
+    }
